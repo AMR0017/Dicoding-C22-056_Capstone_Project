71422e00908db7518dd9f221b58c327b2d788d02
AMR0017 <azhar.mr12@gmail.com>
2022-11-23 22:19:26 +0800

    add homepage layout

--- app/src/main/java/com/example/dicoding_c22_056_capstone_project/Homepage/MainActivity.kt
+++ app/src/main/java/com/example/dicoding_c22_056_capstone_project/Homepage/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dicoding_c22_056_capstone_project
+package com.example.dicoding_c22_056_capstone_project.Homepage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dicoding_c22_056_capstone_project.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
